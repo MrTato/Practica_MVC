1942586ba7f48f4a12472c2e635dbfbc6a031f08
AlexC01 <alexcuadraxxx@gmail.com>
2020-08-28 14:05:13 -0600

    Modelo de bayardo

---
+++ Practica_MVC/Controllers/EmpleadoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica_MVC.Models;
+namespace Practica_MVC.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        List<EmpleadoCLS> listaEmpleado = null;
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            using (var database = new MANTENIMIENTOEntities())
+            {
+                listaEmpleado = (from E in database.Empleado
+                                 select new EmpleadoCLS
+                                 {
+                                     id_empleado = E.id_empleado,
+                                     salario_por_hora = (float)E.salario_por_hora,
+                                     nombre = E.nombre
+                                 }).ToList();
+            }
+            return View(listaEmpleado);
+        }
+    }
+}
---
+++ Practica_MVC/Models/EmpleadoCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica_MVC.Models
+{
+    public class EmpleadoCLS
+    {
+        [Display(Name = "ID Empleado")]
+        public int id_empleado { get; set; }
+        [Display(Name = "Salario Laboral")]
+        public float salario_por_hora { get; set; }
+
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+    }
+}

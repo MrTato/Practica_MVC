aba624324b02d88543a9d04729348d348bf7b7bc
Luz Clarita <luzpm@uamv.edu.ni>
2020-08-27 22:40:28 -0600

    Todo

---
+++ Practica_MVC/Controllers/TipoEquipoController.cs
@@ -0,0 +1,31 @@
+﻿using Practica_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica_MVC.Controllers
+{
+    public class TipoEquipoController : Controller
+    {
+        List<TipoEquipo> ListaTipoEquipo = null;
+        // GET: TipoEquipo
+        public ActionResult Index()
+        {
+
+            using (var bd = new MANTENIMIENTOEntities())
+            {
+                ListaTipoEquipo = (from E in bd.Tipo_Equipo
+                                   select new TipoEquipo
+                                   {
+                                       tipo_equipo = E.tipo_equipo1,
+                                       medida = E.medida
+                                   }).ToList();
+
+
+            }           
+            return View(ListaTipoEquipo);
+        }
+    }
+}
---
+++ Practica_MVC/Models/TipoEquipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica_MVC.Models
+{
+    public class TipoEquipo
+    {
+        [Display(Name = "Tipo Equipo")]
+       public String tipo_equipo { get; set; }
+        [Display(Name = "Medida")]
+        public String medida { get; set; }
+    }
+}

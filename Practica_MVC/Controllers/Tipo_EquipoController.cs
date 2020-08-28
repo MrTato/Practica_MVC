d3eee64fb9cce43e3a3b193b06d26a5e6ce38db4
AlexC01 <alexcuadraxxx@gmail.com>
2020-08-28 14:22:39 -0600

    modelo de luz

---
+++ Practica_MVC/Controllers/Tipo_EquipoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica_MVC.Models;
+
+namespace Practica_MVC.Controllers
+{
+    public class Tipo_EquipoController : Controller
+    {
+        List<Tipo_EquipoCLS> ListaTipoEquipo = null;
+        // GET: TipoEquipo
+        public ActionResult Index()
+        {
+
+            using (var bd = new MANTENIMIENTOEntities())
+            {
+                ListaTipoEquipo = (from E in bd.Tipo_Equipo
+                                   select new Tipo_EquipoCLS
+                                   {
+                                       tipo_equipo = E.tipo_equipo1,
+                                       medida = E.medida
+                                   }).ToList();
+
+
+            }
+            return View(ListaTipoEquipo);
+        }
+    }
+}
